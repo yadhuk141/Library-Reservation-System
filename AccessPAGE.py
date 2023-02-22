0aa100da7b5dc16e679e5cf50566ae5320b50d7d
Yadhu.Sathya <yadhu.sathya>
2023-02-22 09:30:01 +0530

    Used collections

---
+++ AccessPAGE.py
@@ -0,0 +1,151 @@
+'''
+Title: Online library reservation system  
+
+Author: Yadhu krishnan S 
+
+Created on: 02/02/2023 
+
+Last Modified Date: 22/02/2023 
+
+Reviewed by:  
+
+Reviewed on:
+'''
+import csv
+import re
+from AdminMENU import admin_Menu as am
+from Home import h 
+
+class Access:
+    def __init__(self):
+        pass
+    def login(self):
+        count=0
+        while True:
+            flag=True
+            if count>=3:
+                print("Too many attempts!")
+                return
+            ch=input("Continue Log in? (y/n):")
+            if ch=='n' or ch=='N':
+                return
+            userid=input("Enter username: ")
+            pw=input("Enter password:")
+            with open('Users.csv',"r") as users:
+                read=csv.reader(users,delimiter=",")
+                for r in read:
+                    try:
+                        if r[0]==userid and r[1]==pw:
+                            print("Valid credentials!!!\n")
+                            flag=False
+                            h.Menu(r[0])
+                            break
+                    except:
+                        continue
+            if flag:
+                print("Incorrect username or password!!!")
+                count+=1
+                print("TRY AGAIN!!!\n")
+
+    def clr_blank(self):
+        with open('temp.csv', newline='') as in_file:
+            with open('Users.csv', 'a', newline='') as out_file:
+                writer = csv.writer(out_file)
+                for row in csv.reader(in_file):
+                    if row:
+                        writer.writerow(row)                
+                        
+    def verify(self,u):
+        with open('Users.csv','r') as f:
+            read=csv.reader(f,delimiter=",")
+            for i in read:
+                if i:
+                    if i[0]==u:
+                        return True
+
+    def register(self):
+        new_user=[]
+        while True:
+            temp=input("Enter user id:")
+            if re.match(r"[A-Za-z\s]+",temp):
+                check=self.verify(temp)
+                if check:
+                    print("Username already exists try again!!!\n")
+                    continue
+                print("Valid username")
+                new_user.append(temp)
+                break
+            else:
+                print("Invalid username try again!!!\n")
+        while True:
+            temp=input("Enter new password:")
+            if re.match(r"^(?=.*[\d])(?=.*[A-Z])(?=.*[a-z])(?=.*[@#$])[\w\d@#$]{6,12}$", temp):
+                print("Valid password")
+                new_user.append(temp)
+                break
+            else:
+                print("Invalid password try again!!!\n")
+                print("Password should contain atleast one Upper case, one number and one special character")
+        while True: 
+            temp=input("Enter address:")
+            if re.match(r"[A-Za-z\s]+",temp):
+                new_user.append(temp)
+                print("Valid address")
+                break
+            else:
+                print("Invalid address try again!!!\n")
+        try:
+            with open('temp.csv',"w") as users:
+                writer=csv.writer(users)
+                writer.writerow(new_user)
+        except Exception:
+            print("An error occured, please try again\n")
+            return
+        else:
+            self.clr_blank()
+            print("User registered succesfully!!!\n")
+            return
+
+    def admin(self):
+        count=0
+        admin="admin"
+        password="12345"
+        while True:
+            if count>=3:
+                print("Too many attempts!!!\n")
+                return
+            ch=input("Continue? (y/n):")
+            if ch=='n' or ch=='n':
+                return
+            adminid=input("Enter admin ID: ")
+            adminpass=input("Enter admin password:")
+            if admin==adminid and password==adminpass:
+                print("Valid credentials!!!\n")
+                am.mainMenu()
+                break
+            else:
+                print("Invalid Credentials try again!!!\n")
+                count+=1
+                
+
+#MAIN
+obj=Access()
+flag=True
+print("----------------Welcome-----------------")
+while flag:
+    print("\n1.Login\n2.Register\n3.Admin Login\n4.Exit\n")
+    try:
+        ch=int(input("Enter choice:"))
+    except Exception:
+        print("Invalid choice try again")
+        continue
+    if ch==1:
+        obj.login()
+    elif ch==2:
+        obj.register()
+    elif ch==3:
+        obj.admin()
+    elif ch==4:
+        flag=False
+    else:
+        print("\nInvalid choice try again\n")
---
+++ AdminMENU.py
@@ -0,0 +1,201 @@
+import csv
+
+class Admin_menu:
+    def __init__(self):
+        self.books=[["Python","RD","u"],["Devops","JD","u"]]
+
+    """ def clr_blank(self):
+        with open('temp.csv', newline='') as in_file:
+            with open('Books.csv', 'a', newline='') as out_file:
+                writer = csv.writer(out_file)
+                for row in csv.reader(in_file):
+                    if row:
+                        writer.writerow(row) """
+    
+    def Search(self,title):
+        flag=True 
+        """ with open('Books.csv',"r") as books:
+            read=csv.reader(books,delimiter=",") """
+
+        for r in self.books:
+            if r[0]==title:
+                print("Book found!!\n")
+                print("TITLE: ",r[0])
+                print("AUTHOR: ",r[1])
+                if r[2]=='r':
+                    print("STATUS: RESERVED\n")
+                else:
+                    print("STATUS: UNRESERVED\n")
+                flag =False
+                return flag
+        return flag
+
+    def remove_user(self):
+        with open("Users.csv","r") as inp,open('temp.csv','w') as out:
+            csv_inp=csv.reader(inp)
+            csv.writer(out).writerows(csv_inp)
+        user_name=input("Enter username to be removed: ")
+        
+        reader = csv.reader(open("temp.csv", "r"), delimiter=',')
+        f = csv.writer(open("Users.csv", "w"))
+        flag=True
+        for line in reader:
+            
+            if user_name not in line:
+                if line:
+                    f.writerow(line)
+            flag=False
+                #print line
+        if flag:
+            print("User not present in database\n")
+        else:
+            print("User Removed\n")
+        return
+
+    def add_books(self):
+        list1=[]
+
+        temp=input("Enter book title:")
+        list1.append(temp)
+        temp=input("Enter book author:")
+        list1.append(temp)
+        temp=input("Enter reservation status:")
+        list1.append(temp)
+        self.books.append(list1)
+
+        """ try:
+            with open('temp.csv',"w") as users:
+                writer=csv.writer(users)
+                writer.writerow(books)
+        except Exception:
+            print("An error occured, please try again\n")
+            return
+        else:
+            self.clr_blank()
+            print("\nBook added succesfully!!!\n") """
+
+        return
+
+    def remove_books(self):
+        """  with open("Books.csv","r") as inp,open('temp.csv','w') as out:
+            csv_inp=csv.reader(inp)
+            csv.writer(out).writerows(csv_inp) """
+
+        book=input("Enter title of book to be removed:")
+
+        """ reader = csv.reader(open("temp.csv", "r"), delimiter=',')
+        f = csv.writer(open("Books.csv", "w")) """
+
+        for line in self.books:
+            flag=True
+            if book in line:
+                self.books.pop(self.books.index(line))
+                flag=False
+                break
+        if flag:
+            print("Book not present in database\n")
+        else:
+            print("\nBook Removed\n")
+            #self.clr_blank()
+        return
+
+    def Return_books(self):
+        """ TEXTFILE = open("temp.csv", "w")
+        TEXTFILE.truncate()
+        TEXTFILE.close()"""
+
+        title=input("Enter title: ") 
+        flag=self.Search(title)
+        if flag:
+            print("Book does not exist in the library")
+            return
+        else:
+            flag =True
+            """ with open("Books.csv",'r') as books:
+                with open("temp.csv","a") as temp:
+                    read=csv.reader(books,delimiter=",")
+                    writer = csv.writer(temp)
+                    for r in read:
+                        if r: """
+            for r in self.books:
+                if r[0]==title and r[2]=="u" and flag:
+                    print("The book is unreserved\n")
+                    return
+                elif r[0]==title and r[2]=='r' and flag:
+                    r[2]="u"
+                    #writer.writerow(r)
+                    print("Book is returned!!!")
+                    flag=False
+
+                """ else:
+                    writer.writerow(r)  
+                    self.clr_blank()  """                   
+
+
+    def view_users(self):
+        print("\nUsername\tCity")
+        print("-------------------")
+        with open("Users.csv","r")as u:
+            read=csv.reader(u)
+            for r in read:
+                if r:
+                    print(r[0],"\t",r[2])
+        print("\n")
+    def Book_manage(self):
+        print("------BOOK MANAGEMENT--------")
+        while True:
+            print("1.Add books\n2.Remove books\n3.Return book\n4.Go back")
+            try:
+                ch=int(input("Enter choice:"))
+            except Exception:
+                print("Invalid choice try again")
+                continue
+            if ch==1:
+                self.add_books()
+            elif ch==2:
+                self.remove_books()
+            elif ch==3:
+                self.Return_books()
+            elif ch==4:
+                return
+            else:
+                print("\nInvalid choice try again\n")
+
+    def User_manage(self):
+        print("------USER MANAGEMENT--------")
+        while True:
+            print("1.Remove user\n2.Go back")
+            try:
+                ch=int(input("Enter choice:"))
+            except Exception:
+                print("Invalid choice try again")
+                continue
+            if ch==1:
+                self.remove_user()
+            elif ch==2:
+                return
+            else:
+                print("\nInvalid choice try again\n")
+
+
+    def mainMenu(self):
+        print("------ADMIN MENU--------")
+        while True:
+            print("1.Books management\n2.Users management\n3.View Users\n4.Go back")
+            try:
+                ch=int(input("Enter choice:"))
+            except Exception:
+                print("Invalid choice try again")
+                continue
+            if ch==1:
+                self.Book_manage()
+            elif ch==2:
+                self.User_manage()
+            elif ch==3:
+                self.view_users()
+            elif ch==4:
+                return
+            else:
+                print("\nInvalid choice try again\n")
+                
+admin_Menu=Admin_menu() 
